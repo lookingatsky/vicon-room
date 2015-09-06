067c0f495c87088806df0822162521d732bda025
钟浩林 <zhonghaolin@zailicai.com>
2015-09-06 11:16:32 +0800

    更改样式

--- invo-master/app/controllers/HelpController.php
+++ invo-master/app/controllers/HelpController.php
@@ -4,7 +4,7 @@ class HelpController extends ControllerBase
 {
     public function initialize()
     {
-        //$this->view->setTemplateAfter('main');
+        $this->view->setTemplateAfter('main');
         Phalcon\Tag::setTitle('帮助中心');
         parent::initialize();
     }
