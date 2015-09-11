dd6153dd9ae0c88dd057197475934af76efff915
钟浩林 <zhonghaolin@zailicai.com>
2015-09-11 16:30:41 +0800

    测试

--- invo-master/app/controllers/VoteController.php
+++ invo-master/app/controllers/VoteController.php
@@ -46,7 +46,8 @@ class VoteController extends ControllerBase
 					$openid = $dataJson['openid'];
 					$this->view->setVar("dataJson",$dataJson);
 					$this->view->setVar("openid",$openid);
-					$ACCESS_TOKEN = "2D6t8FiF6cwngpdwyAvW7A81ruZuWBoJjoqrxn4jF9r7D3VnqGaP9ZYR7sc1KiDW6-cT_xyDtsGHjcsIV8e4zaI-lpY8FPn0Vi5bZcifVZE";
+					//$ACCESS_TOKEN = "2D6t8FiF6cwngpdwyAvW7A81ruZuWBoJjoqrxn4jF9r7D3VnqGaP9ZYR7sc1KiDW6-cT_xyDtsGHjcsIV8e4zaI-lpY8FPn0Vi5bZcifVZE";
+					$ACCESS_TOKEN = $dataJson['access_token'];
 					$OPENID = $openid;
 					$accessTokenUrl_ =  "https://api.weixin.qq.com/cgi-bin/user/info?access_token=".$ACCESS_TOKEN."&openid=".$OPENID."&lang=zh_CN";
 					
